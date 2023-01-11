328302b38b3f94cf90c4aae87d2198563f021651
faraz <shm.faraz@15gmail.com>
2023-01-11 22:44:53 +0530

    First Commit

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import v, { d, b, c } from './module2.mjs'
+
+console.log(v)
+console.log(d)
+console.log(b)
+console.log(c)
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Abhisek"
+const b = "Malay"
+const c = "Durga"
+const d = "Jaswanth"
+
+export default a;
+export { b };
+export { c };
+export { d };
