aa8334e5de09f9c5d997a0f711c46570327ce390
faraz <shm.faraz@15gmail.com>
2023-01-13 17:19:36 +0530

    auto dismissing alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 //import logo from './logo.svg';
+// import About from './About';
 import './App.css';
 import Navbar from './Navbar';
 import Textform from './Textform';
+import React, { useState } from 'react';
+import Alert from './Alert';
 
 function App() {
+  const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState('light');
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500)
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode has been enabled", "success")
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Dark mode has been disabled", "success")
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="AboutTextUtils" />
+      {/* <Navbar title="TextUtils" aboutText="AboutTextUtils" /> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <Textform heading="Enter the text to analyze below" />
+        {/* <About /> */}
+        <Textform heading="Enter the text to analyze below" mode={mode} />
       </div>
       {/* <Navbar /> */}
     </>
--- src/Textform.js
+++ src/Textform.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 export default function Textform(props) {
 
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
 
     const handleOnChange = (event) => {
         console.log("on change function called")
@@ -15,14 +15,28 @@ export default function Textform(props) {
         setText(newText)
     }
 
+    const handleLowClick = () => {
+        console.log("Lowercase was clicked")
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
     return (
         <>
             <div className="mb-3">
-                <h1>{props.heading}</h1>
+                <h1 style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>{props.heading}</h1>
                 <div className='mb-3'>
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} style={{ background: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? '#042743' : 'white' }} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+            </div>
+            <div className='container my-3'>
+                <h2 style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>Text Summary</h2>
+                <p style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>{text.split(" ").length} words and {text.length} characters</p>
+                <p style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>{text.split(" ").length * 0.008} minutes read</p>
+                <h2 style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>Preview</h2>
+                <p style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>{text}</p>
             </div>
         </>
     )
